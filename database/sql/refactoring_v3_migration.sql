a57807fb54747a15377cbf5d83bb5a081f60c7a3
Chaoran Chen <mail@chaoran-chen.de>
2022-01-31 23:23:26 +0100

    Update migration script

--- database/sql/refactoring_v3_migration.sql
+++ database/sql/refactoring_v3_migration.sql
@@ -1,5 +1,5 @@
-
-insert into z_test_metadata (test_id, ethid, order_date, zip_code, canton, city, is_positive, comment)
+-- Migrating the "normal" samples coming from Viollier
+insert into z_test_metadata (test_id, ethid, order_date, zip_code, canton, city, is_positive, purpose, comment)
 select
   'viollier/' || sample_number,
   ethid,
@@ -8,10 +8,58 @@ select
   canton,
   city,
   is_positive,
+  purpose,
   comment
 from viollier_test;
 
--- TODO merge non_viollier_plate into test_sample
+
+-- Migrating the samples from the non_viollier_test table:
+-- The table contains mostly Team-w samples, but also a few samples from other other labs, including Viollier.
+-- The Viollier samples in the table do not have a sample number. We disconnected the sequences from their
+-- sample numbers because we are not sure about their mapping.
+-- The following helper query shows the labs in the non_viollier_test table and whether a sample number is known.
+--     select
+--       covv_orig_lab,
+--       sample_number is not null,
+--       count(*)
+--     from
+--       non_viollier_test
+--     group by
+--       covv_orig_lab,
+--       sample_number is not null;
+-- The results:
+--     Viollier AG,false,69
+--     "Clinical Laboratory, Vetsuisse Faculty, University of Zurich",false,8
+--     MCL Medizinische Laboratorien Hauptstandort Niederwangen,true,4
+--     "Institute for Molecular Health Science, ETH Zurich",false,48
+--     Institut Central des Hôpitaux Valaisans ICHV/ZIWS Service des Maladies Infectieuses,false,1
+--     "Department of Dermatology, University of Zürich, University of Zürich Hospital, Switzerland ",false,48
+--     labor team w AG,true,3814
+-- For the samples without a known sample number, we will "unknown"
+insert into z_test_metadata (test_id, ethid, order_date, zip_code, canton, city, is_positive, purpose, comment)
+select
+  (case -- For the test id, I think that it is better to use a short name for the labs
+    when covv_orig_lab = 'Viollier AG' then 'viollier'
+    when covv_orig_lab = 'Clinical Laboratory, Vetsuisse Faculty, University of Zurich' then 'vetsuisse'
+    when covv_orig_lab = 'MCL Medizinische Laboratorien Hauptstandort Niederwangen' then 'mcl_niederwangen'
+    when covv_orig_lab = 'Institute for Molecular Health Science, ETH Zurich' then 'mhs_eth'
+    when covv_orig_lab = 'Department of Dermatology, University of Zürich, University of Zürich Hospital, Switzerland '
+      then 'dermatology_uzh'
+    when 'labor team w AG' then 'team_w'
+  end) || '/' ||
+  coalesce(sample_number, 'unknown'),
+  si.ethid,
+  nvt.order_date,
+  nvt.zip_code,
+  nvt.canton,
+  null,
+  true,
+  null,
+  nvt.comment
+from
+  non_viollier_test nvt
+  left join sequence_identifier si on nvt.sample_name = si.sample_name;
+
 
 insert into z_extraction_plate (
   extraction_plate_name, gfb_number, fgcz_name, health2030, left_lab_or_received_metadata_date,
@@ -29,12 +77,17 @@ select
 from viollier_plate;
 
 
+-- After 1 August 2021, the new procedure where plates with only positive samples are sent to the sequencing
+-- labs is running smoothly. I.e., the samples from a "viollier plate" will also be sequenced together.
+-- For the samples before that, we don't have this information. If we want to fill the table for those samples, we need
+-- to ask the sequencing labs.
 insert into z_sequencing_plate (sequencing_plate_name, sequencing_center)
 select viollier_plate_name, sequencing_center
 from viollier_plate vp
 where left_viollier_date >= '2021-08-01';
 
 
+-- Before 7 June 2021, we had the old procedure. Samples resulted that it shall be marked as "old".
 insert into z_test_plate_mapping (
   test_id, extraction_plate, extraction_plate_well, extraction_e_gene_ct, extraction_rdrp_gene_ct,
   sample_type, old_sample
@@ -52,6 +105,7 @@ from
   join viollier_plate vp on vtvp.viollier_plate_name = vp.viollier_plate_name
 where vp.left_viollier_date <= '2021-06-07';
 
+
 insert into z_test_plate_mapping (
   test_id, extraction_plate, extraction_plate_well, extraction_e_gene_ct, extraction_rdrp_gene_ct,
   sample_type, old_sample
@@ -68,46 +122,3 @@ from
   viollier_test__viollier_plate vtvp
   join viollier_plate vp on vtvp.viollier_plate_name = vp.viollier_plate_name
 where vp.left_viollier_date > '2021-06-07';
-
-insert into z_consensus_sequence (
-  sample_name, sequencing_center, sequencing_batch, seq_aligned, ethid
-)
-select
-  sample_name,
-  sequencing_center,
-  sequencing_batch,
-  seq,
-  ethid
-from consensus_sequence;
-
-insert into z_consensus_sequence_meta (
-  sample_name,
-
-  coverage_mean, coverage_median, r1_basequal, r2_basequal, rejreads, alnreads, insertsize, consensus_n,
-  consensus_lcbases, consensus_diffbases, snvs, snvs_majority, qc_result,
-
-  diagnostic_divergence, diagnostic_excess_divergence, diagnostic_number_n, diagnostic_number_gaps,
-  diagnostic_clusters, diagnostic_gaps, diagnostic_all_snps, diagnostic_flagging_reason,
-
-  nextclade_clade, nextclade_qc_overall_score, nextclade_qc_overall_status, nextclade_total_gaps,
-  nextclade_total_insertions, nextclade_total_missing, nextclade_total_mutations, nextclade_total_non_acgtns,
-  nextclade_total_pcr_primer_changes, nextclade_alignment_start, nextclade_alignment_end,
-  nextclade_alignment_score, nextclade_qc_missing_data_score, nextclade_qc_missing_data_status,
-  nextclade_qc_missing_data_total, nextclade_qc_mixed_sites_score, nextclade_qc_mixed_sites_status,
-  nextclade_qc_mixed_sites_total, nextclade_qc_private_mutations_cutoff, nextclade_qc_private_mutations_excess,
-  nextclade_qc_private_mutations_score, nextclade_qc_private_mutations_status,
-  nextclade_qc_private_mutations_total, nextclade_qc_snp_clusters_clustered, nextclade_qc_snp_clusters_score,
-  nextclade_qc_snp_clusters_status, nextclade_qc_snp_clusters_total, nextclade_errors,
-
-  pango_lineage, pango_probability, pango_learn_version, pango_status, pango_note
-)
-select
-  cs.sample_name,
-
-  coverage, null, r1_basequal, r2_basequal, rejreads, alnreads, insertsize, consensus_n,
-       consensus_lcbases, diffb
-
-
-from
-  consensus_sequence cs
-  left join consensus_sequence_nextclade_data nd on cs.sample_name = nd.sample_name
