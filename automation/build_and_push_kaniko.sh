b475256f7728e3754b8b6951734f95459d483819
Uwe Schmitt <uwe.schmitt@id.ethz.ch>
2022-04-28 20:57:11 +0200

    build all images

---
+++ automation/build_and_push_kaniko.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+set -eu
+
+# Constants
+IMAGE_NAME=registry.ethz.ch/sars_cov_2/harvester-database-and-automation
+
+component=$1
+suffix=_dev
+
+echo build $component with suffix $suffix
+echo .
+
+cp -r ../database $component/database
+
+# Fetch repository of pangolin lineages
+if [[ $component = pangolin_lineage_importer ]]; then
+    cp -R ../pangolin pangolin_lineage_importer
+fi
+
+if [[ $component = spsp_transferer ]]; then
+    # Fetch repository of SPSP transfer tool
+    cp -R ../transfer-tool spsp_transferer
+fi
+
+
+/kaniko/executor --context ${component} --dockerfile ${component}/Dockerfile --destination $IMAGE_NAME:${component}${suffix} --cache=false
