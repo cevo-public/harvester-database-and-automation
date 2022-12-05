abd94981e115c26de85dbd61ed27539e66408171
jhennig <john.hennig@id.ethz.ch>
2022-12-05 11:42:02 +0000

    Version-tag Docker images with Git tag (tasks#163)
    This allows for easy rollback to a previous version in production.

---
+++ .gitlab/build_components.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+# Fail on any error.
+set -euo pipefail
+
+# Build version-tagged Docker image for each component.
+for component in \
+    bag_meldeformular_importer \
+    bag_meldeformular_dashboard_importer \
+    gisaid_importer \
+    gisaid_api_importer \
+    java \
+    nextclade_importer \
+    owid_global_cases_importer \
+    pangolin_lineage_importer \
+    sequence_diagnostic_importer \
+    viollier_metadata_receiver
+do
+    echo .
+    echo .
+    echo .
+    echo "-------------------------------------------------------------------------------"
+    echo Building component: $component
+    echo .
+    cp -r ${CI_PROJECT_DIR}/database ${CI_PROJECT_DIR}/automation/$component/database
+
+    if [[ $component = pangolin_lineage_importer ]]; then
+        # Fetch repository of pangolin lineages, provided as an artifact.
+        cp -R ${CI_PROJECT_DIR}/pangolin ${CI_PROJECT_DIR}/automation/$component
+    fi
+
+    if [[ $component = spsp_transferer ]]; then
+        # Fetch repository of SPSP transfer tool, provided as an artifact.
+        cp -R ${CI_PROJECT_DIR}/transfer-tool ${CI_PROJECT_DIR}/automation/$component
+    fi
+
+    /kaniko/executor \
+        --context ${CI_PROJECT_DIR}/automation/$component \
+        --dockerfile ${CI_PROJECT_DIR}/automation/$component/Dockerfile \
+        --destination $CI_REGISTRY_IMAGE:$component-$CI_COMMIT_TAG \
+
+done
